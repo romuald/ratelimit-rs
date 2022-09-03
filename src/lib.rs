d670599d4eb33e186c916d15513774cdae12d563
Romuald Brunet <romuald@chivil.com>
2022-09-03 09:24:27 +0200

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
-use std::time::{Duration, Instant};
+use std::time::Duration;
+
+#[cfg(not(test))]
+use std::time::Instant;
+
+#[cfg(test)]
+use mock_instant::Instant;
+//use fake_instant::FakeClock as Instant;
 
 use std::collections::HashMap;
 use std::convert::TryFrom;
@@ -21,26 +28,66 @@ impl Entry {
             timestamps: vec![0; BLOCK_SIZE],
         }
     }
+
+    // now is the difference since epoch
+    fn rebase(&mut self, now: u32) -> u32{
+        let epoch = self.epoch;
+
+        if now < u32::MAX / 2 {
+            return now;
+        }
+
+        println!(
+            "Rebase epoch={:?}, now={:?}",
+            epoch, now
+        );
+        println!("Values: {:?}", self.timestamps);
+
+        let min :u32 = match self.timestamps.iter().filter(|x| **x > 0).min() {
+            Some(x) => *x - 1,
+            None => 0,
+        };
+
+
+        let new_epoch = self.epoch + Duration::from_millis(min.into());
+        for timestamp in self.timestamps.iter_mut() {
+            if *timestamp > 0 {
+                *timestamp -= min;
+            }
+        }
+        self.epoch = new_epoch;
+        println!("new values: {:?}", self.timestamps);
+
+
+        println!("min: {:?}, new={:?}", min, new_epoch);
+
+        return now - min;
+    }
+
     /// Hits the ratelimit
     /// size is the maximum number of hits allowed
     /// duration is the duration in milliseconds for which the hits are allowed
     fn hit(&mut self, size: u32, duration: u32) -> bool {
         let diff = Instant::now().duration_since(self.epoch);
-        let now = u32::try_from(diff.as_millis()).unwrap();
-    
+        let mut now = u32::try_from(diff.as_millis()).unwrap();
+
         let index = usize::try_from(self.index).unwrap();
-        if index == self.timestamps.len() {
-            let max = usize::try_from(size).unwrap();
-            let increment = cmp::min(BLOCK_SIZE, max - index);
-            self.timestamps.extend(vec![0; increment]);
+
+        {
+            if index == self.timestamps.len() {
+                let max = usize::try_from(size).unwrap();
+                let increment = cmp::min(BLOCK_SIZE, max - index);
+                self.timestamps.extend(vec![0; increment]);
+            }
         }
 
-        let last = self.timestamps[index];
-        //println!("ts: {:?}, now: {:?}, diff: {:?}", self.timestamps, now, diff);
-        let delta = now - last;
+        now = self.rebase(now);
 
-        // println!("delta: {:?} index {:?}, last {:?}, period: {:?}", delta, self.index, last, period);
-        if last > 0 && delta < duration {
+        let previous = self.timestamps[index];
+        //println!("ts: {:?}, now: {:?}, diff: {:?}", self.timestamps, now, diff);
+
+        // println!("delta: {:?} index {:?}, previous {:?}, period: {:?}", delta, self.index, previous, period);
+        if previous > 0 && (now - previous) < duration {
             return false;
         } else {
             self.timestamps[index] = now;
@@ -64,6 +111,8 @@ pub struct Ratelimit {
 
 impl Ratelimit {
     pub fn new(hits: u32, duration: u32) -> Ratelimit {
+        // Assert hits > 0
+        // Assert duration < 49 days
         Ratelimit {
             hits: hits,
             duration: duration,
@@ -72,10 +121,8 @@ impl Ratelimit {
     }
 
     pub fn hit(&mut self, name: &String) -> bool {
-        return match self.entries.get_mut(name) {
-            Some(entry) => {
-                entry.hit(self.hits, self.duration)
-            }
+        match self.entries.get_mut(name) {
+            Some(entry) => entry.hit(self.hits, self.duration),
             None => {
                 let mut new_entry = Entry::new();
                 new_entry.hit(self.hits, self.duration);
@@ -83,5 +130,109 @@ impl Ratelimit {
                 true // assumes that we are not limited to 0 hits
             }
         }
+    }
+
+    pub fn cleanup(&mut self) {
+        let min = Instant::now() - Duration::from_millis(1000 + u64::from(self.duration));
+
+        self.entries.retain(|_, v| {
+            println!("{:?}", v.timestamps);
+            let index = usize::try_from(v.index).unwrap();
+
+            let last = match index {
+                0 => v.timestamps.len() - 1,
+                _ => index - 1,
+            };
+
+            let last_ts = v.epoch + Duration::from_millis(u64::from(v.timestamps[last]));
+
+            last_ts > min
+        });
+    }
+}
+
+#[cfg(test)]
+
+mod test {
+    use super::*;
+    use mock_instant::{MockClock};
+
+    #[test]
+    fn test_base_process() {
+        // Basic test "suite", hitting the ratelimit within a
+        // specific timeframe will either return true or false
+        let ms1 = Duration::from_millis(1);
+        let root = ms1.clone();
+        let rl_duration_ms = 2_000;
+        let rl_duration = Duration::from_millis(rl_duration_ms.into());
+
+        MockClock::set_time(root);
+
+        let mut rl = Ratelimit::new(10, rl_duration_ms);
+        let st = String::from("test");
+
+        // 10 hits OK in 1 second
+        for _ in 0..10 {
+            assert_eq!(rl.hit(&st), true);
+
+            MockClock::advance(Duration::from_millis(10));
+        }
+
+        // still not OK
+        assert_eq!(rl.hit(&st), false);
+
+        // still not OK less than 2 seconds from the start
+        MockClock::set_time(root + rl_duration - ms1);
+        assert_eq!(rl.hit(&st), false);
+
+        // 
+        MockClock::advance(Duration::from_millis(1));
+        assert_eq!(rl.hit(&st), true);
+    }
+
+    #[test]
+    fn test_overflow() {
+        // 
+        MockClock::set_time(Duration::from_millis(200));
+
+        let mut rl = Ratelimit::new(1, 86400 * 1000);
+        let st = String::from("test");
+
+        MockClock::set_time(Duration::from_millis(200));
+
+        for _i in 0..70 {
+            assert!(rl.hit(&st));
+            MockClock::advance(Duration::from_secs(86400));
+        }
+    }
+
+    #[test]
+    fn test_cleanup() {
+        let root = Duration::from_millis(86_400_000);;
+        let rl_duration_ms = 60_000;
+
+        MockClock::set_time(root);
+
+        let mut rl = Ratelimit::new(10, rl_duration_ms);
+
+        rl.hit(&String::from("foo"));
+        rl.hit(&String::from("bar"));
+
+        MockClock::advance(Duration::from_millis(59_000));
+        rl.hit(&String::from("bar"));
+
+        rl.cleanup();
+        assert_eq!(rl.entries.len(), 2);
+
+        MockClock::advance(Duration::from_millis(3_000));
+
+        rl.cleanup();
+        assert_eq!(rl.entries.len(), 1);
+
+        MockClock::advance(Duration::from_millis(59_000));
+
+        rl.cleanup();
+        assert_eq!(rl.entries.len(), 0);
+
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,83 @@
 #![cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
+use std::io;
 use ratelimit_rs::Ratelimit;
 use std::time::{Duration, Instant};
 
 use std::thread::sleep;
 
+struct InputReader {
+    input: std::io::Stdin,
+}
 
-fn main() {
-    let mut rl = Ratelimit::new(19, 100);
+fn inputreader() -> InputReader {
+    InputReader { input: io::stdin() }
+}
 
+impl Iterator for InputReader {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut buffer = String::from("");
+
+        match self.input.read_line(&mut buffer) {
+            Ok(_) => {
+                let value = buffer.trim();
+
+                match value.len() {
+                    0 => None,
+                    _ => Some(value.to_string()),
+                }
+            }
+            Err(_) => None,
+        }
+    }
+}
+
+
+fn main() -> io::Result<()> {
+    let mut rl = Ratelimit::new(5, 10000);
+
+
+    for keyname in inputreader() {
+        if rl.hit(&keyname) {
+            println!("OK for {:?}", keyname);
+        } else {
+            println!("Not OK for {:?}", keyname);
+        }
+    };
+
+    /*
+    loop {
+        buffer.clear();
+
+        match stdin.read_line(&mut buffer) {
+            Ok(n) => {
+                let value = buffer.trim();
+                if value.len() == 0 { break }
+            }
+            Err(_e) => break,
+        }
+    }
+    */
+
+    Ok(())
+}
+fn demo() {
+    
+    let mut rl = Ratelimit::new(50, 100);
     let mut b: bool;
 
     let name = &String::from("hello");
 
-    for i in 0..20 {
+    for _i in 0..3 {
         b = rl.hit(&name);
         println!("@{:?}, {:?}", line!(), b);
     }
+    sleep(Duration::from_millis(150));
 
+    b = rl.hit(&name);
+    println!("@{:?}, {:?}", line!(), b);
 
+    b = rl.hit(&name);
+    println!("@{:?}, {:?}", line!(), b);
 }
