731f593efdfbfe46f26e133d70699b49ca00c4bb
Romuald Brunet <romuald@chivil.com>
2022-10-16 16:50:06 +0200

    move memcache handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 mod collection;
-mod memcache_handler;
+mod handlers;
 mod ratelimit;
 
 pub use crate::collection::RatelimitCollection;
 pub use crate::ratelimit::{Ratelimit, RatelimitInvalidError};
 
-pub use crate::memcache_handler::StreamHandler;
+pub use crate::handlers::memcache::StreamHandler;
