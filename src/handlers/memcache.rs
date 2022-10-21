c7130c69aee16ff7f946c57e03cddbdb85dd50f5
Romuald Brunet <romuald@chivil.com>
2022-10-21 12:15:32 +0200

    Try starting mock to test memcache handler

--- src/handlers/memcache.rs
+++ src/handlers/memcache.rs
@@ -16,6 +16,49 @@ enum Command {
     INCR,
 }
 
+pub struct MockTcpStream {
+    read_data: Vec<u8>,
+    write_data: Vec<u8>,
+}
+use async_std::io::{Read, Write};
+use std::cmp::min;
+use std::pin::Pin;
+use futures::io::Error;
+use futures::task::{Context, Poll};
+impl Read for MockTcpStream {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        _: &mut Context,
+        buf: &mut [u8],
+    ) -> Poll<Result<usize, Error>> {
+        let size: usize = min(self.read_data.len(), buf.len());
+        buf[..size].copy_from_slice(&self.read_data[..size]);
+        Poll::Ready(Ok(size))
+    }
+}
+
+impl Write for MockTcpStream {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        _: &mut Context,
+        buf: &[u8],
+    ) -> Poll<Result<usize, Error>> {
+        self.write_data = Vec::from(buf);
+
+        Poll::Ready(Ok(buf.len()))
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, _: &mut Context) -> Poll<Result<(), Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_close(self: Pin<&mut Self>, _: &mut Context) -> Poll<Result<(), Error>> {
+        Poll::Ready(Ok(()))
+    }
+}
+impl Unpin for MockTcpStream {}
+
+
 pub struct StreamHandler {
     stream: TcpStream,
     ratelimit: Arc<Mutex<Ratelimit>>,
